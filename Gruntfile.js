16fe9cb2b6c8b8851dc497b51e1bb561024453c7
郑尚姿 <33643927+zhengshangzi@users.noreply.github.com>
2020-02-26 08:29:07 +0800

    Add files via upload

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+/*global module true */
+module.exports = function (grunt) {
+  grunt.initConfig({
+    eslint: {
+      options: {
+        configFile: '.eslintrc.json'
+      },
+      target: ['*.js']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-eslint');
+
+  grunt.registerTask('default', ['eslint']);
+}
