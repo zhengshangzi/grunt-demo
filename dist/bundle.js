53c8c238b0ed1d481b2023abff83866c173aa73b
郑尚姿 <33643927+zhengshangzi@users.noreply.github.com>
2020-03-04 08:33:40 +0800

    Add files via upload

---
+++ dist/bundle.js
@@ -0,0 +1,30 @@
+$(function() {
+  var $width = $('#width'),
+      $height = $('#height'),
+      $btnCal = $('#calculate'),
+      $perimeter = $('#perimeter'),
+      $area = $('#area');
+
+  $btnCal.click(function(){
+    var w = $width.val(),
+        h = $height.val();
+
+    var r = new Rectangle(w, h);
+
+    $perimeter.val(r.perimeter());
+    $area.val(r.area());
+  });
+});
+/* exported Rectangle */
+function Rectangle(width, height) {
+    var w = Number(width),
+        h = Number(height);
+  
+    this.area = function() {
+      return w * h;
+    };
+  
+    this.perimeter = function() {
+      return 2 * (w + h);
+    };
+  }
